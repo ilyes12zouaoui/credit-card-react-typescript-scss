0a69e0ba18ec02c45abbf54caeb6678885e17c61
ilyes12zouaoui <ilyes.zouaoui@esprit.tn>
2019-10-07 00:07:44 +0200

    Updates

---
+++ precache-manifest.38871c3d1d819c0a29688c16a5268b6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f42df5b79682635bdc19c16d04a887",
+    "url": "/credit-card-react-typescript-scss/index.html"
+  },
+  {
+    "revision": "f489c77efd2601c3613b",
+    "url": "/credit-card-react-typescript-scss/static/css/main.5ed6dfa5.chunk.css"
+  },
+  {
+    "revision": "bf7d2034ad228df0f4e9",
+    "url": "/credit-card-react-typescript-scss/static/js/2.e0b48a43.chunk.js"
+  },
+  {
+    "revision": "f489c77efd2601c3613b",
+    "url": "/credit-card-react-typescript-scss/static/js/main.ec155e8e.chunk.js"
+  },
+  {
+    "revision": "08c49e88b1636e0de290",
+    "url": "/credit-card-react-typescript-scss/static/js/runtime-main.0f7d8262.js"
+  },
+  {
+    "revision": "c8be26a7669eb550abdf54ed92e522d9",
+    "url": "/credit-card-react-typescript-scss/static/media/ship.c8be26a7.png"
+  }
+]);
